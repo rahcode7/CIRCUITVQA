34cb6fad9c9989484f474e37785a4c5f7cf7811c
rahcode7 <rahul008mehta@gmail.com>
2024-06-01 12:46:45 +0530

    codes

--- src/data-prep/00-data-download.py
+++ src/data-prep/00-data-download.py
@@ -1,7 +1,7 @@
 #!pip install roboflow
 
 from roboflow import Roboflow
-rf = Roboflow(api_key="xQbOkJIaW7bKCU5vyobC")
+rf = Roboflow(api_key="roboflow_api_key")
 project = rf.workspace("development-tohnm").project("cghd-full-supplemented")
 dataset = project.version(13).download("yolov8")
 
